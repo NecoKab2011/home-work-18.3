fa3d4e392ce85ae462a67488977bf250e2b88326
NecoKab2011 <142300074+NecoKab2011@users.noreply.github.com>
2024-12-22 16:07:19 +0100

    dom2

---
+++ js/dom2.js
@@ -0,0 +1,44 @@
+const listElement = document.querySelectorAll("#categories > li");
+console.log(`У списку ${listElement.length} категорії.`);
+
+listElement.forEach((list) => {
+    const titleElement = list.querySelector("h2").textContent;
+    const itemElement = list.querySelectorAll("ul > li").length;
+    console.log(`Категорія: ${titleElement}`);
+    console.log(`Кількість елементів: ${itemElement}`);
+  });
+
+
+const ingredients = [
+  "Картопля",
+  "Гриби",
+  "Часник",
+  "Помідори",
+  "Зелень",
+  "Приправи",
+];
+
+const listIngr = document.querySelector("#ingredients");
+for (const i of ingredients) {
+  listIngr.insertAdjacentHTML("beforeend", `<li>${i}</li>`);
+}
+
+const images = [
+  {
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
+];
+
+const listImg = document.querySelector("#gallery");
+for (const { url, alt } of images) {
+  listImg.insertAdjacentHTML("beforeend", `<img src="${url}" alt="${alt}">`);
+}
